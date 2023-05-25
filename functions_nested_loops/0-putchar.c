bb94eaad32153b59b86d19b6be2d9ab30744fc71
JoMcCray03 <Jo.mccray2003@gmail.com>
2023-05-25 01:52:12 -0700

    l

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main -  prints _putchar, followed by a new line
+ * main - Entry point
  * Description: 'prints _putchar to stdout'
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
